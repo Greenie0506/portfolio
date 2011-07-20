9ebf2029fc7e708d975631aadd731dc652833c5d
Nicholas Greenfield <greenie0506@gmail.com>
2011-07-19 22:46:23 -0400

    Social Media, Date, Deleting a Post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def index
+    #twitter_api
     search = Twitter::Search.new
     recent_tweets = search.from('nickpgreenfield').result_type("recent")
     @handle_tweets = recent_tweets.per_page(3).fetch
 
+    #blog
     @post = Post.all
 
+    #date
+    @time = Time.now.strftime("%A, %B %d, %Y at %l%p")
   end
 
  def show
@@ -24,7 +28,13 @@ class ApplicationController < ActionController::Base
     if @post.save
       redirect_to(@post)
     end
+  end
 
+  def destroy
+  @post = Post.find(params[:id])
+  @post.destroy
+
+  redirect_to root_path
   end
 
 end
