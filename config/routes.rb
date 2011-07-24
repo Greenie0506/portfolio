0c61e94e86bd8e8d79d2da7fdeeab8bd6a430f8c
Nicholas Greenfield <greenie0506@gmail.com>
2011-07-24 13:59:24 -0400

    headers to sections

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Portfolio::Application.routes.draw do
 
-  resources :home, :only => [:new, :create]
+  resources :home, :only => [:create]
 
   resources :posts, :controller => 'post'
   match '/new', :to => 'post#new'
